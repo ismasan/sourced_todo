28643921094a62ec54a36e70aa0f554d727d7445
Ismael Celis <ismaelct@gmail.com>
2025-02-09 16:29:38 +0000

    version navigation buttons

--- ui/components/event_list.rb
+++ ui/components/event_list.rb
@@ -2,19 +2,32 @@ module Components
   class EventList < Phlex::HTML
     def initialize(events:, seq: nil, href_prefix: 'todo-lists', reverse: true)
       @events = events
+      @first_seq = @events.first&.seq
+      @last_seq = @events.last&.seq
       @events = @events.reverse if reverse
-      @seq = seq
+      @seq = seq || @last_seq
       @href_prefix = href_prefix
     end
 
     def view_template
       div id: 'event-list' do
         div class: 'header' do
-          h2 { 'History' }
+          h2 { "Current version: #{@seq}" }
           if @events.any?
             p(data: { signals: '{"showPayloads": false}' }) do
               button(class: 'toggle-payloads', data: { on: { click: '$showPayloads = !$showPayloads' } }) do
                 span(data: { text: '$showPayloads ? "Hide Payloads" : "Show Payloads"' })
+              end
+              disabled_back = @first_seq == @seq
+              disabled_forward = @last_seq == @seq
+
+              button(disabled: disabled_back,
+                     data: { on: { click: %(@get('/#{@href_prefix}/#{@events.first.stream_id}/#{@seq - 1}')) } }) do
+                '←'
+              end
+              button(disabled: disabled_forward,
+                     data: { on: { click: %(@get('/#{@href_prefix}/#{@events.first.stream_id}/#{@seq + 1}')) } }) do
+                '→'
               end
             end
           end
